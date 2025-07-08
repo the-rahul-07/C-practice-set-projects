e680ad55e55cc6c298d1a0d1752535db87f5a762
Rahul <rahulgupta2019007@gmail.com>
2025-07-08 12:20:13 +0530

    first chapter

---
+++ chapter1/01_first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a; // declaring variable
+    a = 6; // intializing
+    printf("The output of the program is %d\n", a);
+    printf("Hello World");
+    return 0;
+}
---
+++ chapter1/input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Give the value of a : ");
+    scanf("d%", &a);
+    return 0;
+}
