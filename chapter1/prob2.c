1ac835a6c300f56f52f3ce2ab3bc2c2f58f88ad3
Rahul <rahulgupta2019007@gmail.com>
2025-07-08 17:17:03 +0530

    problem2

---
+++ chapter1/prob2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int r;
+    int h;
+    printf("Enter the radius:");
+    scanf("%d", &r);
+    printf("Enter the height:");
+    scanf("%d", &h);
+    printf("Area of the circle with radius %d is %f", r, 3.14 * r * r);
+    printf("\nvolume of the cylinder with radius %d and height %d is : %f", r, h, 3.14 * r * r * h);
+    return 0;
+}
