bd7e32fb53250923e2a9fd7585a84cec8011719d
Rahul <rahulgupta2019007@gmail.com>
2025-07-08 16:51:03 +0530

    problem1

---
+++ chapter1/prob1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int l, b;
+    printf("enter length :");
+    scanf("%d", &l);
+
+    printf("enter breadth :");
+    scanf("%d", &b);
+
+    printf("area of rectangle is : %d", l * b);
+    return 0;
+}
