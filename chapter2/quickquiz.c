683a1136eb70a43f1a98e7d9870591ffcbb58cca
Rahul <rahulgupta2019007@gmail.com>
2025-07-19 11:12:38 +0530

    changes in quickquiz

--- chapter2/quickquiz.c
+++ chapter2/quickquiz.c
@@ -3,6 +3,7 @@
 int main()
 {
     int k = 3.0 / 9;
-    printf("value of k = %d", k);
+    printf("value of k = %d", k); /*value of k is actually 0.333 but since k is int,
+    it cannot store floats & 0.333 is demoted to 0 */
     return 0;
 }
