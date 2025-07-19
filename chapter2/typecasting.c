7c56a0b9c890238f02b58d2a985181ad7c472780
Rahul <rahulgupta2019007@gmail.com>
2025-07-19 12:08:34 +0530

    typecasting

---
+++ chapter2/typecasting.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 2;
+    float b = 56.25;
+    a = (int)b; // convert the datatype to int
+    printf("%d\n", a);
+
+    // typecasting syntax -->
+    // a(variable which has datatype tht has to be converted) = (datatype to be converted) b(vari whose datatype to be converted)
+    return 0;
+}
