e3a8697fa2efd513dbd057b971071d4ae637df11
Rahul <rahulgupta2019007@gmail.com>
2025-09-18 19:27:53 +0530

    adding changes

--- chapter2/quickquiz.c
+++ chapter2/quickquiz.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+    // checking adding and commiting
     int k = 3.0 / 9;
     printf("value of k = %d", k); /*value of k is actually 0.333 but since k is int,
     it cannot store floats & 0.333 is demoted to 0 */
