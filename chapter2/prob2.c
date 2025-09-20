92ed9077b15e191f4aae34be9b1e9754155756e8
Rahul <rahulgupta2019007@gmail.com>
2025-09-21 00:11:19 +0530

    prob2

---
+++ chapter2/prob2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    int a;
+    printf("Enter number to check weather divisible by 97 or not :");
+    scanf("%d", &a);
+    int b;
+    if (a % 97 == 0)
+    {
+        printf("%d is divisible by 97", a);
+    }
+    else
+    {
+        printf("%d is not divisible by 97", a);
+    }*/
+
+    // 2ND METHOD VERY SIMPLE ONE
+
+    // int a = 454545;
+    // printf("value of a%97 is %d", a % 97);
+
+    // so if the value is 0 it means the number is divisible
+    // if not equal to 0 thn not divisible
+    return 0;
+}
