41005fd442410c5941aed3d64baf4b3d4b14ebbd
Rahul <rahulgupta2019007@gmail.com>
2025-09-21 20:42:54 +0530

    ternary

---
+++ chapter3/ternary.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    // condition ? expression if ture : expression if false
+    int a = 300;
+    int b = 120;
+    a > b ? printf("a is greater") : printf("b is greater");
+    return 0;
+}
