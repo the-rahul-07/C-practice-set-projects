cdbe99cf6ca1885cafafa85e2b77c0ebb5ececa3
Rahul <rahulgupta2019007@gmail.com>
2025-09-21 21:16:02 +0530

    switch

---
+++ chapter3/switch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter a : ");
+    scanf("%d", &a);
+
+    switch (a)
+    {
+    case 1:
+        printf("you entered 1\n");
+        break;
+    case 2:
+        printf("you entered 2\n");
+        break;
+    case 3:
+        printf("you entered 3\n");
+        break;
+    case 4:
+        printf("you entered 4\n");
+        break;
+    default:
+        printf("Nothing matched");
+    }
+    return 0;
+}
