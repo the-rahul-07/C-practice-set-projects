edde9c0868e9d1a826fb48f30b0aa9730a3a811f
Rahul <rahulgupta2019007@gmail.com>
2025-09-21 15:48:53 +0530

    if_else

---
+++ chapter3/if_else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    // if else statement
+    if (a > 10)
+    {
+        printf("we are inside if\n");
+        printf("you age is greater thn 10\n");
+    }
+    else
+    {
+        printf("you age is not greater than 10");
+    }
+
+    return 0;
+}
