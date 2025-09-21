403a121071e68a332217633cd24d89ed0967c9b2
Rahul <rahulgupta2019007@gmail.com>
2025-09-21 21:29:31 +0530

    prob1.c

---
+++ chapter3/prob1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    if (a = 0)
+    {
+        printf("I am 11\n");
+        printf("%d", a);
+    }
+    else
+    {
+        printf("I am not 11");
+    }
+    return 0;
+}
