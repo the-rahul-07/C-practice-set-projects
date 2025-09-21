58a1e9191df0277e68f957ff005df9055b01d9a2
Rahul <rahulgupta2019007@gmail.com>
2025-09-21 15:41:37 +0530

    conditional

---
+++ chapter3/conditional.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 15;
+    // if statement
+    if (a > 10)
+    {
+        printf("we are inside if\n");
+        printf("you age is greater thn 10\n");
+    }
+    return 0;
+}
