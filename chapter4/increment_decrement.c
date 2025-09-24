a242facb7c6f744855433d2f4b7d55024e3f3893
Rahul <rahulgupta2019007@gmail.com>
2025-09-24 23:17:51 +0530

    incre_decre

--- chapter4/increment_decrement.c
+++ chapter4/increment_decrement.c
@@ -4,12 +4,17 @@ int main()
 {
     int i = 0;
     printf("The value of i is %d\n", i);
-    i = i + 5;
-    printf("The value of i is %d\n", i);
-    printf("The value of i is %d\n", i++);
-    printf("The value of i is %d\n", i);
+    i = i + 5;                             // 5
+    printf("The value of i is %d\n", i);   // 5
+    printf("The value of i is %d\n", i++); // 5
+    printf("The value of i is %d\n", i);   // 6
 
-    // i++ increment 1st then prints i (pre increment operator)
-    // ++i print 1st and then increment i (post increment operator)
+    // i++ print i 1st and then increment i (post increment operator)
+    // ++i increment i 1st then prints i (pre increment operator)
+
+    i += 2; // same as i = i + 2;
+    printf("The value of i is %d\n", i);
+    i += 3; // same as i = i + 3;
+    printf("The value of i is %d\n", i);
     return 0;
 }
